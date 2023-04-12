d795b68ff971540fd4d7c286ebdefd56c36c4451
RyuisangGo <107903342+YUSUNOK@users.noreply.github.com>
2023-04-12 20:22:07 +0900

    [level 0] Title: 각도기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/각도기.js
+++ 프로그래머스/lv0/120829. 각도기/각도기.js
@@ -1,18 +1,5 @@
 function solution(angle) {
-    var answer = 0;
-    if(angle > 0 && angle < 90){
-        answer = 1;
-    }
-    else if(angle === 90) {
-        answer = 2;
-    }
-    else if(angle> 90 && angle < 180){
-        answer =3;
-    }
-    else if (angle === 180) {
-        answer =4;
-    }
-   
-    return answer;
+    let arr = [0, 90, 91, 180];
+    const answer = arr.filter(x => angle>=x).length;
     return answer;
 }
