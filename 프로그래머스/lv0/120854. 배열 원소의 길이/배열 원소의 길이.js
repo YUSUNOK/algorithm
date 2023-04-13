103d40635516f1884b2f805ed91c2cf580a6ddaa
RyuisangGo <107903342+YUSUNOK@users.noreply.github.com>
2023-04-14 00:30:55 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.js
+++ 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.js
@@ -1,5 +1,3 @@
 function solution(strlist) {
-    var answer = Array(strlist.length);
-    strlist.map((x, i) => answer[i] = x.length);
-    return answer;
+    return strlist.map((x, i) => x.length);
 }
