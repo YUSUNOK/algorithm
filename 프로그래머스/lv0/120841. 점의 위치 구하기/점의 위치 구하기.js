a7641864937f3b228ea837ed4a263e2736a8a7c0
RyuisangGo <107903342+YUSUNOK@users.noreply.github.com>
2023-04-13 11:06:47 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -0,0 +1,22 @@
+function solution(dot) {
+     var answer = 0;
+    if(dot[0] > 0){
+        // 1,4
+        if(dot[1] > 0){
+            answer = 1;
+        }
+        else{
+            answer = 4;
+        }
+    } else{
+        // 2,3
+       if(dot[1] > 0){
+            answer = 2;
+        }
+        else{
+            answer = 3;
+        }
+    }
+   
+    return answer;
+}
