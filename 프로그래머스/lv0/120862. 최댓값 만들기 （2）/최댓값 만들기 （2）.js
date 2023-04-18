3f3b082552719664eca0d9adbc7f98df8521aa39
RyuisangGo <107903342+YUSUNOK@users.noreply.github.com>
2023-04-18 19:05:05 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,4 +1,4 @@
 function solution(numbers) {
     numbers.sort((x,y) => x - y);
-    return numbers[0] * numbers[1] > numbers[numbers.length -1] * numbers[numbers.length -2] ? numbers[0] * numbers[1] : numbers[numbers.length -1] * numbers[numbers.length -2];
+    return Math.max(numbers[0] * numbers[1] , numbers[numbers.length -1] * numbers[numbers.length -2]);
 }
