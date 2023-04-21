dd6cb766a3aed0ee5c2a3fbfc816e5e38e35e028
RyuisangGo <107903342+YUSUNOK@users.noreply.github.com>
2023-04-21 09:53:06 +0900

    [level 0] Title: 가위 바위 보, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
@@ -1,7 +1,8 @@
 function solution(rsp) {
-    // 2 -> 0
-    // 0 -> 5
-    // 5 -> 2
-    var answer = [5, undefined, 0, undefined, undefined, 2];
-    return  [...rsp].map(x => x = answer[x]).join("");
+   var answer = {
+       2 : 0,
+       0 : 5,
+       5 : 2,
+   }
+   return [...rsp].map(x => answer[x]).join("");
 }
