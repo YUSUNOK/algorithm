5c3628e9307419c1913f2ab82d99e42172506a71
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-04-25 10:10:48 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -1,6 +1,3 @@
 function solution(s) {
-   const countArr = Array(26).fill(0);
-    [...s].forEach(x => countArr[x.codePointAt() - 97] +=1);
-    const indexArr = countArr.map((x,i)=> x === 1 ? i : -1).filter(x => x > -1);
-    return indexArr.map(x => String.fromCharCode(x + 97)).join("");
+    return [...s].filter(x => s.indexOf(x) === s.lastIndexOf(x)).sort().join("");
 }
