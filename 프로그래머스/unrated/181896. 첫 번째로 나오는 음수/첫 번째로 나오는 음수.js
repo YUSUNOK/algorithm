d18c5e923ebe34b53aac62e9877e615ef5eea314
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-04-25 18:53:32 +0900

    [unrated] Title: 첫 번째로 나오는 음수, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
+++ 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
@@ -1,4 +1,4 @@
 function solution(num_list) {
-    const booleanNum_list = num_list.map(x => x < 0);
-    return booleanNum_list.includes(true) ? booleanNum_list.indexOf(true) : -1;
+   // 조건에 부합하는 첫번째 인덱스를 추출 : findIndex
+    return num_list.findIndex((x) => x < 0);
 }
