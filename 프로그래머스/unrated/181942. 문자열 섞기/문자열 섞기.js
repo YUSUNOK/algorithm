58ced13c188b38cebe06f95c08b8d8448fc10803
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-04-29 22:07:24 +0900

    [unrated] Title: 문자열 섞기, Time: 0.04 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.js
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.js
@@ -1,6 +1,3 @@
 function solution(str1, str2) {
-    let answer = Array(str1.length + str2.length).fill(0);
-    [...str1].map((x, i) => answer[i * 2] = x);
-    [...str2].map((x, i) => answer[i * 2 + 1] = x);
-    return answer.join("");
+    return [...str1].map((x, i) => x + str2[i]).join("");
 }
