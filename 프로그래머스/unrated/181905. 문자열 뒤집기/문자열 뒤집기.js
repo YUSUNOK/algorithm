e533251217737c407274b82afaae6fd7f58f325c
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-05-05 22:22:34 +0900

    [unrated] Title: 문자열 뒤집기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181905. 문자열 뒤집기/문자열 뒤집기.js
+++ 프로그래머스/unrated/181905. 문자열 뒤집기/문자열 뒤집기.js
@@ -1,6 +1,5 @@
 function solution(my_string, s, e) {
-    const answer = [...my_string];
-    const inputString = [...my_string.slice(s, e+1)].reverse().join("");
-    answer.splice(s, inputString.length, inputString);
-    return answer.join("");
+    const removeString = my_string.substring(s, e+1);
+    let answer = my_string.replace(removeString, [...removeString].reverse().join(""));
+    return answer;
 }
