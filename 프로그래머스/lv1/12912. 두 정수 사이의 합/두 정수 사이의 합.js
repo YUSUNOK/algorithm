6354919f6ee12b30596703eb0e7011391e36fe98
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-05-07 03:34:37 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -1,4 +1,3 @@
 function solution(a, b) {
-    const length = Math.abs(a-b) + 1;
-    return length % 2 === 0 ? (a + b) * Math.floor(length / 2) :(a + b) * Math.floor(length / 2) + (a + b) /2 ;
+    return (a + b) * (Math.abs(b-a) + 1) / 2;
 }
