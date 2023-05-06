b1faed6f4c8ed1ebfa2763b4fb07382f4250e3a6
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-05-07 07:26:37 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,15 +1,7 @@
 function solution(left, right) {
-    let answer = 0;
-    const mathematicalFactorsCount = {};
-    for(let i = left ; i <= right; i ++){
-        for(let j = 1 ; j<= Math.sqrt(i) ; j++){
-            if(i % j !== 0) continue;
-            let plusValue = ~~(i / j) === j ? 1 : 2;
-             mathematicalFactorsCount[i] = (mathematicalFactorsCount[i] || 0) + plusValue;
-        }
-    }
-    for(let number in mathematicalFactorsCount){
-        mathematicalFactorsCount[number] % 2 === 0 ? answer += Number(number) : answer -= Number(number);
+    let answer =0;
+    for(let i = left ; i <= right ; i++){
+     Math.sqrt(i) % 1 === 0 ? answer -= i : answer +=i;
     }
     return answer;
 }
