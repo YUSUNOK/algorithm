3da32ac63abd28ca93ebd6e3cbd8e793eeb79efd
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-05-07 05:22:45 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,3 +1,3 @@
 function solution(numbers) {
-    return Array(10).fill(0).map((x, i) => x + i).filter((x) => !numbers.includes(x)).reduce((prev, curr) => prev + curr);
+    return 45 - numbers.reduce((prev, curr) => prev + curr);
 }
