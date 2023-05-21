6f3d7591472b12887d799a9dca75297a2aa4a2cd
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-05-21 23:04:16 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.js
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -1,8 +1,5 @@
 function solution(common) {
-    let differentValueList = [];
-    for(let i = 0 ; i < common.length - 1 ; i++){
-        differentValueList.push(common[i+1] - common[i]);
-    }
-    const differentValueCount = new Set(differentValueList);
-    return differentValueCount.size === 1 ? common[common.length - 1] + differentValueList[0] : common[common.length - 1] * (common[1] / common[0]);
+   return common[1] - common[0] === common[2] - common[1] ?
+     common.pop() + common[1] - common[0] :
+     common.pop() * (common[1] / common[0]);
 }
