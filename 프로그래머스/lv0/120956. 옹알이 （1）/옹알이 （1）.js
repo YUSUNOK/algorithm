3e20f958d64082d8fe77e3e2a9a9ec8651a99754
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-06-09 01:51:20 +0900

    [level 0] Title: 옹알이 (1), Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -0,0 +1,12 @@
+function solution(babbling) {
+    const babbleToCan = ["aya", "ye", "woo", "ma"];
+    for(let babble of babbleToCan) {
+        babbling = babbling.map((x) => {
+          if(x.includes(babble)) return x.replace(babble,'+');
+            return x;
+        })
+    }
+    return babbling.filter((x) => x.replaceAll('+','') === '').length;
+}
+  
+    
