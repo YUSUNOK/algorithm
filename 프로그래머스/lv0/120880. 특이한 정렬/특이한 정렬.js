7c70e388471f85c6eda9a24f258f54d2ea2edddd
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-06-20 23:10:36 +0900

    [level 0] Title: 특이한 정렬, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.js
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.js
@@ -1,9 +1,3 @@
 function solution(numlist, n) {
-    const newArr = [];
-    numlist.map(x => {
-       return x > n ? newArr.push([1, Math.abs(x-n)]) : newArr.push([0, Math.abs(x-n)]);
-    });
-    newArr.sort((x,y) => x[1] - y[1] || y[0] - x[0]);
-    return newArr.reduce((prev, curr) => curr[0] === 1 ? [...prev, n + curr[1]] : [...prev, n - curr[1]], []);
-   
+    return numlist.sort((x,y) => Math.abs(x-n) - Math.abs(y-n) || y - x);
 }
