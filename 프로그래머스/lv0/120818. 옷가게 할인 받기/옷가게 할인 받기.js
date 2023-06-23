c09a49cd59a21d8ccb1c18dd0db1c14864fcaad9
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-06-23 22:08:11 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -1,26 +1,6 @@
-function solution(price) {
-    var arr = [
-        {
-            price : 10,
-            percent : 0,
-        }
-        ,
-        {
-            price : 100000,
-            percent : 0.05,
-        },
-        {
-            price : 300000,
-            percent : 0.1,
-        },
-        {
-            price : 500000,
-            percent : 0.2,
-        },
-        ];
-    
-    var filterArr = arr.filter(x => x.price <= price);
-    return ~~(price - price * filterArr[filterArr.length -1].percent);
-    
-    
+const solution = (price) => {
+    if(price >= 500000) return Math.trunc(price - price * 0.01 * 20);
+    if(price >= 300000) return Math.trunc(price - price * 0.01 * 10);
+    if(price >= 100000) return Math.trunc(price - price * 0.01 * 5);
+    return price;
 }
