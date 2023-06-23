a2ed09ef90ee96fdb404c45f2875092364ab18b3
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-06-23 23:02:19 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -1,6 +1,6 @@
-function solution(dot) {
-     var answer = 0;
-    dot.reduce((prev, curr) => prev * curr) > 0 ? dot[1] > 0? answer =1 : answer = 3 : dot[0] > 0 ? answer =4 : answer= 2; 
-   
-    return answer;
+const solution = (dot) => {
+    switch(dot[0] * dot[1] < 0){
+        case true : return dot[0] < 0 ? 2 : 4; 
+        case false : return dot[0] < 0 ? 3 : 1;
+    }
 }
