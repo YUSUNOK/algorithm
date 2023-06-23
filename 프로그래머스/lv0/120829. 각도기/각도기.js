2eba5b545a5e48caf2c1ffdade0e0f752c2f9b6e
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-06-23 20:24:23 +0900

    [level 0] Title: 각도기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/각도기.js
+++ 프로그래머스/lv0/120829. 각도기/각도기.js
@@ -1,5 +1,6 @@
-function solution(angle) {
-    let arr = [0, 90, 91, 180];
-    const answer = arr.filter(x => angle>=x).length;
-    return answer;
+const solution = (angle) => {
+    switch(angle % 90) {
+        case 0 : return angle === 90 ? 2 : 4;
+        default : return angle < 90 ? 1 : 3;       
+    }
 }
