b4adabdf77a7c5b4f0cfa37a55a9b9bdd8d6450b
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-06-23 22:38:56 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,6 +1,6 @@
 const solution = (num_list) => {
     let result = [0,0];
     num_list
-    .map(x => x % 2 === 0 ? result[0] += 1 : result[1]+= 1);
+    .map(x => result[x%2] += 1);
     return result;
 }
