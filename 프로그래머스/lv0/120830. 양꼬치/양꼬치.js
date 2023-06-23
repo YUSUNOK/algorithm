65426c63fee94fb88c4f5d738fbbbc36e35af45e
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-06-23 21:15:34 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
@@ -1,5 +1,1 @@
-function solution(n, k) {
-     
-    var answer = (k - parseInt(n/10)) * 2000 + n * 12000 ;
-    return answer;
-}
+const solution = (n, k) => n * 12000 + (k - Math.trunc(n / 10)) * 2000;
