4ec82233a708103f2fea3e1d2eca145111ff8339
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-06-25 17:36:39 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -1,7 +1,7 @@
-function solution(my_string, num1, num2) {
-    const [charA, charB] = [my_string[num1], my_string[num2]];
-    let answerArr = [...my_string];
-    answerArr[num1] = charB;
-    answerArr[num2] = charA;
-    return answerArr.join("");
+const solution = (my_string, num1, num2) => {
+    const [firstStr, secondStr] = [my_string[num1], my_string[num2]];
+    let strToArr = [...my_string];
+    strToArr[num1] = secondStr;
+    strToArr[num2] = firstStr;
+    return strToArr.join('');
 }
