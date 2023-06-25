85b9ab18f55b64206a4ca556019e8bb4c9ef2129
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-06-25 10:50:34 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.18 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -1,3 +1,8 @@
-function solution(n) {
-    return Array(n).fill(1).map((x, i) => x+i).filter(x => n % x ===0).length;
+const solution = (n) => {
+    let answer = 0;
+    for(let i = 1 ; i <= Math.sqrt(n) ; i++){
+        if(i === Math.sqrt(n) && n % i === 0) answer += 1;
+        else if(n % i === 0) answer += 2;
+    }
+    return answer;
 }
