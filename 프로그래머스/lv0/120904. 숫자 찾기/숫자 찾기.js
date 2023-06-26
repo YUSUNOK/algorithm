f38bdd602a77476e7db4ea7c1790995daa3f739f
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-06-26 11:23:28 +0900

    [level 0] Title: 숫자 찾기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.js
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.js
@@ -1,4 +1,4 @@
-function solution(num, k) {
-    const numArr = [...String(num)].map(x => +x);
-    return numArr.includes(k) ? numArr.indexOf(k) + 1 : -1;
+const solution = (num, k) => {
+    const result = [...String(num)].indexOf(String(k));
+    return result < 0 ? result : result + 1;
 }
