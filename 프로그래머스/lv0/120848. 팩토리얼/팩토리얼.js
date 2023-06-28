212a2c788bddeea134fd19a218c6b585be7a696a
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-06-28 23:38:25 +0900

    [level 0] Title: 팩토리얼, Time: 0.07 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.js
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.js
@@ -1,8 +1,9 @@
-function solution(n) {
-    let gop = 1;
-    let index = 1;
-    while(gop * index <= n) {
-        gop *= index++;
+const solution = (n) => {
+    let [preFactorial, nowFactorial] = [1, 1];
+    for(let i = 1; ; i++){
+        nowFactorial = preFactorial * i;
+        if(nowFactorial === n ) return i;
+        if(nowFactorial > n) return i - 1;
+        preFactorial = nowFactorial;     
     }
-    return index - 1;
 }
