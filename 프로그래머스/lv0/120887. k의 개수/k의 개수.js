1789151178da4edb2886d26ced90541a6c6fb4b1
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-07-02 17:57:00 +0900

    [level 0] Title: k의 개수, Time: 9.17 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.js
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.js
@@ -1,2 +1,6 @@
-const solution = (i, j, k) =>
-    [...Array(j-i + 1).fill(i).map((x, i) => x+i).join("")].map(x => +x).filter(x => x === k).length;
+const solution = (i, j , k) => 
+    Array(j-i+1)
+    .fill(i)
+    .map((x, i) =>      
+        [...String(x+i)].filter(x => x === String(k)).length)
+    .reduce((prev, curr) => prev + curr);
