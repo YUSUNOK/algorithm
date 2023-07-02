ef46a736ea9dc91766eb3e0b46dd6a626744c190
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-07-02 18:16:57 +0900

    [level 0] Title: 가까운 수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
@@ -1,4 +1,2 @@
-function solution(array, n) {
-    const absArray = [...array].map(x => Math.abs(x - n));
-    return array.includes(n - Math.min(...absArray)) ? n - Math.min(...absArray) : n + Math.min(...absArray);
-}
+const solution = (array, n) => 
+array.sort((x, y) => Math.abs(x-n) - Math.abs(y-n) || x - y)[0];
