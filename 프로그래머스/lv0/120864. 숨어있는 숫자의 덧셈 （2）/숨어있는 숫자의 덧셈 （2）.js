15d592ea73a5e9c025703ca2f4768266297357eb
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-07-15 16:51:16 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,5 +1,4 @@
-const solution = (my_string) => {
-   const filterNumbers = my_string.replaceAll(/[A-Za-z]/g, '*').split('*').filter((x) => x);
-   return filterNumbers.length === 0 ? 0 : 
-    filterNumbers.reduce((prev, curr) => prev + Number(curr), 0);
-}
+const solution = (my_string) => 
+    my_string
+    .split(/\D+/)
+    .reduce((prev, curr) => prev + Number(curr), 0);
