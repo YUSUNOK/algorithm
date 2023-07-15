37dcc840b71d06afe526ad979d4de65bc15b0778
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-07-15 23:58:26 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
+++ 프로그래머스/lv0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
@@ -1,15 +1,7 @@
-function solution(my_str, n) {
-    var arr = [...my_str];
-    var answer = [];
-    var num = 0;
-    for(let i = 0 ; i < Math.ceil(arr.length / n); i++){
-        if(i === Math.ceil(arr.length / n) -1){
-             answer.push(arr.slice(num).join(""));    
-        }
-        else{
-        answer.push(arr.slice(num , num + n ).join(""));     
-        }
-        num += n;
+const solution = (my_str, n) => {
+    let answer = [];
+    for(let i = 0 ; i < my_str.length; i+=n){
+        answer.push(my_str.slice(i, i+n));
     }
     return answer;
 }
