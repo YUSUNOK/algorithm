7b72720a4f60f6d719d99bce76c5b005c8e77789
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-07-16 00:57:54 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.js
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -1,17 +1,8 @@
-function solution(my_string) {
-    let [answer, removeSpaceArray, PMIndex] = [0,0,[]];
-    removeSpaceArray = my_string.split(" ");
-    let filterNumber = removeSpaceArray.filter((x, i) => {
-        if(x === '+'){
-            PMIndex.push('+');
-        }else if(x === '-'){
-            PMIndex.push('-');
-        }else{
-            return x;
-        }
-    })
-    answer += Number(filterNumber.shift());
-    PMIndex.map((x, i) => x === '+' ? answer += Number(filterNumber[i]) : answer -= Number(filterNumber[i])
-    )
-    return answer;
+const solution = (my_string) => {
+    const removeSpaceArr = my_string.split(' ');
+    return removeSpaceArr.reduce((preValue, currValue, currIndex) =>  {
+        if(currValue === '+') return preValue + Number(removeSpaceArr[currIndex + 1]);
+        if(currValue === '-') return preValue - Number(removeSpaceArr[currIndex + 1]);
+        return preValue;
+    }, Number(removeSpaceArr[0]));
 }
