24b007775d945f9cb913a376efa415ea50d529b9
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-08-07 21:43:43 +0900

    [level 0] Title: 외계어 사전, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
@@ -1,11 +1,4 @@
-function solution(spell, dic) {
-    const spellsSet = new Set(spell);
-    for(let dicItem of dic) {
-        const dicItemSet = new Set(dicItem);
-        if (spellsSet.size !== dicItemSet.size) continue;
-        if ([...spellsSet].every(i => dicItemSet.has(i))) {
-            return 1;
-        }  
-    }
-    return 2;
+const solution = (spell, dic) => {
+    const checkWord = spell.sort().join('');
+    return dic.map(x => [...x].sort().join('')).includes(checkWord) ? 1:2;
 }
