3d717a3ebf5bc4ba26e62bfade284de3bb7ab7ae
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-08-07 23:07:58 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -4,29 +4,20 @@ const solution = (keyinput, board) => {
     for(let direction of keyinput) {
         switch(direction) {
             case 'left':
-                result[0]-=1;
-                if(result[0] < -limitBoardAbs[0]){
-                    result[0] = -limitBoardAbs[0];
-                }
+                if(--result[0] < -limitBoardAbs[0]) result[0] = -limitBoardAbs[0];
                 break;
                 
              case 'down':
-                result[1]-=1;
-                if(result[1] < -limitBoardAbs[1]){
-                    result[1] = -limitBoardAbs[1];
-                }
+                if(--result[1] < -limitBoardAbs[1]) result[1] = -limitBoardAbs[1];
+                
                 break;
            case 'right':
-                result[0]+=1;
-                if(result[0] > limitBoardAbs[0]){
-                    result[0] = limitBoardAbs[0];
-                }
+                if(++result[0] > limitBoardAbs[0]) result[0] = limitBoardAbs[0];
+                
                 break;
             case 'up':
-                result[1]+=1;
-                if(result[1] > limitBoardAbs[1]){
-                    result[1] = limitBoardAbs[1];
-                }
+                if(++result[1] > limitBoardAbs[1]) result[1] = limitBoardAbs[1];
+                
                 break;     
         }
     }
