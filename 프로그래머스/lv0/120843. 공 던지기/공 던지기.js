16a86f9ca3e52e9fa8145657de2c39c1b34eeba8
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-08-07 17:44:52 +0900

    [level 0] Title: 공 던지기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.js
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.js
@@ -1,14 +1,4 @@
 const solution = (numbers, k) => {
-    let index = 0;
-    for(let i = 1; i <= k ; i++) {
-        if(i === 1) continue;
-        index+=2;
-        if(index - (numbers.length - 1) === 1){
-            index = 0;
-        }
-         if(index - (numbers.length - 1) === 2){
-            index = 1;
-        }
-    }
-    return numbers[index];
+    const index = 2*(k-1);
+    return numbers[index % numbers.length];
 }
