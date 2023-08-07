c4e2c61ea58c2f582f0e0b258b8366bccaf97ce5
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-08-07 16:45:17 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.js
+++ 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.js
@@ -1,6 +1,4 @@
-function solution(sides) {
-    const inputRemain = Math.min(...sides);
-    // 가장 긴 변이 들어왔을 때  --> inputMax - inputRemain <  x <= inputMax x의 개수 : inputRemain
-    // 가장 긴 변이 포함되어있지 않았을 때  inputRemain -1 개 
-    return 2 * inputRemain - 1;
+const solution = (sides) => {
+    const smallValue = Math.min(...sides);
+    return 2 * smallValue - 1;
 }
