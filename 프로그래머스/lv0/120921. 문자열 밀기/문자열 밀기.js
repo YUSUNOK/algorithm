583343a9c93613d48630f89af2fec276fd0ed75d
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-08-13 16:44:27 +0900

    [level 0] Title: 문자열 밀기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
@@ -1,12 +1,12 @@
-function solution(A, B) {
-    const aArr = [...A];
-    const bArr = [...B];
-    let count = 0;
-    if(A === B) return 0;
-    for(let i = 0 ; i < aArr.length - 1 ; i++) {
-        aArr.unshift(aArr.pop());
-        count += 1;
-        if(aArr.join("") === B) return count;
+// 문자열의 길이 -1 번 순회해도 b와 동일하지 않으면 -1 
+const solution = (A, B) => {
+    let arrOfA = [...A];
+    if(arrOfA.join('') === B) return 0;
+    for(let i = 1; i <= A.length - 1; i++) {
+        arrOfA.splice(0, 0, arrOfA.pop());
+       if(arrOfA.join('') === B) {
+           return i;
+       }
     }
     return -1;
 }
