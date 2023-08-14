cf2c70349166b2d0b95c59987d5f2f1baecb6b02
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-08-14 16:24:08 +0900

    [level 0] Title: 다항식 더하기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.js
+++ 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.js
@@ -2,7 +2,6 @@ const solution = (polynomial) => {
     let coefficientX = 0;
     let constant = 0;
     
-    let result = '';
     polynomial.split(' + ').forEach((element) => {
         if(element.includes('x')) {
            coefficientX += Number(element.replace('x', '')) || 1;
@@ -10,19 +9,10 @@ const solution = (polynomial) => {
             constant += Number(element);
         }
     })
-    
-    switch(coefficientX) {
-        case 0: 
-            break;
-        case 1:
-            result += 'x';
-            break;
-        default:
-            result += `${coefficientX}x`;
-    }
-    
-    if(coefficientX !== 0 && constant !== 0) result += ' + ';
-    if(constant !== 0)result += `${constant}`;
-    
-    return result;
+
+    let plus = '';
+    if(coefficientX !== 0 && constant !== 0) plus = ' + ';
+    let aboutX = coefficientX === 0 ? '' : coefficientX === 1 ? 'x' : `${coefficientX}x`;
+    let result = [aboutX, plus, constant === 0 ? '' : constant];
+    return result.join('');
 }
