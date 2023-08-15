820918f12593a7c8d77448a3aa5fd0e46a2e085e
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-08-15 15:06:51 +0900

    [level 0] Title: OX퀴즈, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.js
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.js
@@ -1,14 +1,13 @@
-function solution(quiz) {
-    let answer = [];
-    for(let query of quiz) {
-        const [plusMinus, result] = query.split(" = ");
-        if(plusMinus.includes(" - ")){
-            const [a, b]= plusMinus.split(" - ");
-            (Number(a) - Number(b) === Number(result)) ? answer.push("O") : answer.push("X");
-        }else{
-            const [a, b]= plusMinus.split(" + ");
-            (Number(a) + Number(b) === Number(result)) ? answer.push("O") : answer.push("X");
-        }
+const solution = (quiz) => {
+    let result = [];
+    for(let mathExpression of quiz) {
+        let myAnswer = 0;
+        let removeSpaceArr = mathExpression.split(' ');
+        let isPlus = removeSpaceArr[1] === '+' ? true : false;
+        let numbersArr = removeSpaceArr.filter((item) => !isNaN(Number(item)));
+        myAnswer = isPlus ? Number(numbersArr[0]) + Number(numbersArr[1]) :
+        Number(numbersArr[0]) - Number(numbersArr[1]);
+        myAnswer === Number(numbersArr[2]) ? result.push('O') : result.push('X');
     }
-    return answer;
+    return result;
 }
