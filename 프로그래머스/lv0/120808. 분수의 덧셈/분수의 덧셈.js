d17109a57760e47d7c26636725b65e9e8df6d1b8
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-08-16 04:05:15 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.22 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,14 +1,13 @@
-function solution(numer1, denom1, numer2, denom2) {
-    let down = denom1 * denom2;
-    let up = numer1 * denom2 + numer2 * denom1;
-    let smallValue = up <= down ? up : down;
-    let interNumber = 1;
-    for(let i = smallValue ; i >=1 ; i--){
-        if(up % i === 0 && down % i === 0){
-            interNumber = i;
+const solution = (numer1, denom1, numer2, denom2) => {
+    let upValue = numer1 * denom2 + numer2 * denom1;
+    let downValue = denom1 * denom2;
+    
+    for(let i = Math.min(upValue, downValue); i >=2 ; i--) {
+        if(upValue % i === 0 && downValue % i === 0) {
+            upValue /= i;
+            downValue /= i;
             break;
-        } 
+        }
     }
-    var answer = [parseInt(up/interNumber), parseInt(down/interNumber)];
-    return answer;
+    return [upValue, downValue];
 }
