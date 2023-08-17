3ea49e83b842c80764348dcd29e6c674fe1104f3
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-08-18 08:03:43 +0900

    [unrated] Title: 0 떼기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181847. 0 떼기/0 떼기.js
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.js
@@ -1,4 +1,4 @@
-function solution(n_str) {
-    const index = [...n_str].findIndex(x => x !== '0');
-    return [...n_str].splice(index).join("");
+const solution = (n_str) => {
+    const startIndex = [...n_str].findIndex((element) => element !== '0');
+    return n_str.substring(startIndex);
 }
