b0f8097fb0637ea02d91193bc32a63eed5388cae
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-08-17 17:32:29 +0900

    [unrated] Title: 주사위 게임 1, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.js
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.js
@@ -1,9 +1,8 @@
-function solution(a, b) {
-    const oddCount = Number(a % 2) + Number(b % 2);
-    const resultObject ={
-        0 : Math.abs(a-b),
-        1 : 2 * (a + b),
-        2 : a ** 2 + b ** 2,
+const solution = (a, b) => {
+    const oddCount = [a, b].filter((x) => x % 2 === 1).length;
+    switch (oddCount) {
+        case 0: return Math.abs(a-b);
+        case 1: return 2 * (a + b);
+        case 2: return a ** 2 + b ** 2;
     }
-    return resultObject[oddCount];
 }
