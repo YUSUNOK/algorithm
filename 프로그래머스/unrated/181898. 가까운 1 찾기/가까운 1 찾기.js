6eae5bb74ac87ccbc3be9d6c1c936178aedf9158
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-08-18 08:54:57 +0900

    [unrated] Title: 가까운 1 찾기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.js
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.js
@@ -1,3 +1,6 @@
-function solution(arr, idx) {
-    return  arr.indexOf(1, idx);
+const solution = (arr, idx) => {
+    for(let i = idx; i < arr.length ; i++) {
+        if(arr[i] === 1) return i;
+    }
+    return -1;
 }
