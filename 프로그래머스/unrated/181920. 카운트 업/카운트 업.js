f0aa6ae10fe258af60d9ead92547ea88509f7831
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-08-17 15:19:59 +0900

    [unrated] Title: 카운트 업, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181920. 카운트 업/카운트 업.js
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.js
@@ -1,3 +1,3 @@
-function solution(start, end) {
-    return Array(end - start + 1).fill(start).map((x, i) => x + i);;
+const solution = (start, end) => {
+    return Array(end-start+1).fill(start).map((x, i) => x+i);
 }
