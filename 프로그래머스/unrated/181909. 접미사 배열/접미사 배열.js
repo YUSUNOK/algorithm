a4342efa4aa456ea7d32babe0a9a2e89fc4ffa18
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-08-19 02:17:50 +0900

    [unrated] Title: 접미사 배열, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.js
+++ 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.js
@@ -1,7 +1,7 @@
-function solution(my_string) {
+const solution = (my_string) => {
     let answer = [];
-    for(let i = 0 ; i <= my_string.length -1; i++){
-        answer.push(my_string.slice(i));
+    for(let i = 0; i < my_string.length; i++) {
+        answer.push(my_string.substring(i));
     }
     return answer.sort();
 }
