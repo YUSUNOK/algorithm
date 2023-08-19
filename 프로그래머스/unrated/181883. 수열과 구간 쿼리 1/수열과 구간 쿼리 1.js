3fd4e1907bfb21e7e18a36ac00552946a8731fe1
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-08-20 07:52:17 +0900

    [unrated] Title: 수열과 구간 쿼리 1, Time: 7.22 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.js
+++ 프로그래머스/unrated/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.js
@@ -1,8 +1,6 @@
-function solution(arr, queries) {
-    for(let [start, end] of queries) {
-        for(let i = start ; i <=end ; i++){
-            arr[i] += 1;
-        }
+const solution = (arr, queries) => {
+    for(let query of queries) {
+        arr = arr.map((x, i) => i >= query[0] && i <= query[1] ? x+1 : x)
     }
     return arr;
 }
