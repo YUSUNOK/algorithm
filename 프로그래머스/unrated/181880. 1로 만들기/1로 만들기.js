31e082115060ba1b9d49e2797457fb18b4005981
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-08-20 13:29:32 +0900

    [unrated] Title: 1로 만들기, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.js
+++ 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.js
@@ -1,10 +1,10 @@
-function solution(num_list) {
-    let count = 0;
+const solution = (num_list) => {
+    let result = 0;
     for(let num of num_list) {
-        while(num !== 1) {
-          num = Math.floor(num / 2);
-          count++;  
+        while(num !== 1){
+            num = Math.floor(num / 2);
+            result++;
         }
     }
-    return count;
+    return result;
 }
