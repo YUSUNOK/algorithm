86712a877943802cae828740e2b10583eee74b5d
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-08-20 18:16:23 +0900

    [unrated] Title: 정사각형으로 만들기, Time: 0.05 ms, Memory: 38.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181830. 정사각형으로 만들기/정사각형으로 만들기.js
+++ 프로그래머스/unrated/181830. 정사각형으로 만들기/정사각형으로 만들기.js
@@ -1,15 +1,12 @@
-function solution(arr) {
+const solution = (arr) => {
     const row = arr.length;
     const column = arr[0].length;
     if(row > column) {
-        for(let item of arr){
-            item.push(...Array(row-column).fill(0))
-        }
+        arr = arr.map((x) => [...x, ...Array(row-column).fill(0)]);
     }else{
-        const arrToPush = arr[0].map(x => 0);
-        for(let i =0; i <column-row ; i++){
-            arr.push(arrToPush);
+        for(let i = 0; i <column - row; i++) {
+            arr.push(Array(column).fill(0));
         }
     }
-   return arr;
+    return arr;
 }
