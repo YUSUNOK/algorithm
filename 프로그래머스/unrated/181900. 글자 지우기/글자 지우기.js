b82e13c2c9825e6a86334abb0c9b678dc4ff00ac
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-08-20 13:04:31 +0900

    [unrated] Title: 글자 지우기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.js
+++ 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.js
@@ -1,3 +1,3 @@
-function solution(my_string, indices) {
-    return [...my_string].filter((x, i) => !indices.includes(i)).join("");
+const solution = (my_string, indices) => {
+    return [...my_string].filter((x, i) => !indices.includes(i)).join('');
 }
