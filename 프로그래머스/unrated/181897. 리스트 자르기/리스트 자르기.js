9f4e2d5c5ecf86003967ad5322a8358217fb4fe4
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-08-20 15:00:16 +0900

    [unrated] Title: 리스트 자르기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.js
+++ 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.js
@@ -1,8 +1,24 @@
-function solution(n, slicer, num_list) {
-    const [a,b,c] = slicer;
-    if(n === 1) {return num_list.slice(0, b+1)}
-    if(n === 2) {return num_list.slice(a)}
-    if(n === 3) {return num_list.slice(a, b+1)}
-    if(n === 4) {return num_list.filter((x, i) => i >= a && i <= b && (i-a) % c === 0)}
-    
+const solution = (n, slicer, num_list) => {
+    let startNumber;
+    let lastNumber;
+    switch(n) {
+        case 1:
+            startNumber = 0;
+            lastNumber = slicer[1] + 1;
+            break;
+        case 2:
+            startNumber = slicer[0];
+            lastNumber = num_list.length; 
+            break;
+        case 3: 
+            startNumber = slicer[0];
+            lastNumber = slicer[1] + 1;
+            break;
+        case 4:
+            startNumber = slicer[0];
+            lastNumber = slicer[1] + 1;
+            num_list = num_list.filter((x, i) => 
+                (i >= startNumber) && (i < lastNumber) && (((i-startNumber) % slicer[2]) === 0));
+}
+    return n === 4 ? num_list : num_list.slice(startNumber, lastNumber);
 }
