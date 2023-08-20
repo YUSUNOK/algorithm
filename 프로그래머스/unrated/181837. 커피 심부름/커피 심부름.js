f989f0161acb5092bac9ed3b12fb687db9ad45d1
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-08-20 15:54:33 +0900

    [unrated] Title: 커피 심부름, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.js
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.js
@@ -1,20 +1,8 @@
-function solution(order) {
-    const price = {
-        "americano" : 4500,
-        "cafelatte" : 5000,
-        "anything" : 4500,
-    }
-    let answer = 0;
-    for(let item of order) {
-        if(item.includes("anything")) {
-            answer += price.anything;
-        }
-        if(item.includes("americano")) {
-            answer += price.americano;
-        }
-        if(item.includes("cafelatte")) {
-            answer += price.cafelatte;
-        }
-    }
-    return answer;
+const solution = (order) => {
+    let result = 0;
+    order.forEach((x) => {
+        if(x.includes('cafelatte')) result += 5000;
+        else result += 4500;
+    })
+    return result;
 }
