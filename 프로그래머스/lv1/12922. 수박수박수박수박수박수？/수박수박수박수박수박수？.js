4c445282dae6600cee45e3c0647394a1758a1af6
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-08-21 22:57:12 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,3 +1,4 @@
-function solution(n) {
-    return n % 2 === 0 ? '수박'.repeat(n/2) : '수박'.repeat(~~n/2) + '수';
+const solution = (n) => {
+    let answer = '수박'.repeat(Math.floor(n / 2));
+    return n % 2 === 1 ? answer += '수' : answer;
 }
