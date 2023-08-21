586e6a3fc21f73199941f8ccf0a25600c7fbed1f
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-08-21 23:56:44 +0900

    [level 1] Title: 행렬의 덧셈, Time: 8.93 ms, Memory: 67.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -1,11 +1,8 @@
-function solution(arr1, arr2) {
-    let answer = [];
-    for(let i = 0 ; i < arr1.length; i++){
-        let addArray = [];
-        for(let j =0; j <arr1[i].length ; j++){
-            addArray.push(arr1[i][j] + arr2[i][j]);
+const solution = (arr1, arr2) => {
+    for(let i = 0; i < arr1.length; i++){
+        for(let j = 0; j < arr1[i].length; j++) {
+            arr1[i][j] += arr2[i][j];
         }
-        answer.push(addArray);
     }
-    return answer;
+    return arr1;
 }
