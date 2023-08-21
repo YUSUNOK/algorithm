4418ee818aba82045d71b931be12efecd95c7ac7
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-08-21 21:32:38 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,3 +1,3 @@
-function solution(n) {
-    return Math.sqrt(n) % 1 === 0 ? Math.pow(Math.sqrt(n) + 1, 2) : -1;
+const solution = (n) => {
+    return Math.floor(Math.sqrt(n)) === Math.sqrt(n) ? Math.pow(Math.sqrt(n) + 1, 2) : -1;
 }
