3ed6efb4356b6edf127a0b2b2ca7b33f3b907002
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-08-21 22:51:47 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,3 +1,4 @@
-function solution(s) {
-    return s.length % 2 === 1 ? s.substring(~~(s.length / 2), ~~(s.length / 2) + 1) : s.substring(s.length / 2 - 1, s.length / 2 + 1);
+const solution = (s) => {
+    const index = Math.floor(s.length/2);
+    return s.length % 2 === 0 ? s.substring(index-1, index+1) : s[index];
 }
