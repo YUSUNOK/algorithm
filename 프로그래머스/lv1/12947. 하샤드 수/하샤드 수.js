4a9cff91c364b4f7c8634d86301629a188fb4050
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-08-21 21:47:19 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
@@ -1,9 +1,9 @@
-function solution(x) {
-    let n = x;
+const solution = (x) => {
+    let pureX = x;
     let sum = 0;
-    while(x !==0){
+    while(x !== 0) {
         sum += x % 10;
-        x = ~~(x / 10);
+        x = Math.floor(x/10);
     }
-    return n % sum === 0;
+    return !(pureX % sum);
 }
