ab8eb0c6e597fb5ab65bcb95a024f010602d11d5
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-08-22 13:42:52 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,18 @@
+const solution = (s) => {
+    const matchingObj = {
+        'zero': 0,
+        'one': 1,
+        'two': 2,
+        'three': 3,
+        'four': 4,
+        'five': 5,
+        'six': 6,
+        'seven': 7,
+        'eight': 8,
+        'nine': 9
+    }
+    Object.keys(matchingObj).forEach((x, i) => {
+       s = s.replaceAll(x, matchingObj[x]);
+    })
+    return Number(s);
+}
