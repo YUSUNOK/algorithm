7efdeb92fc750b851c959eddfe837051c06ceffc
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-08-23 10:15:07 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -0,0 +1,10 @@
+const solution = (strings, n) => {
+    return strings.sort((x, y) => {
+        if(x[n] > y[n]) return 1;
+        else if(x[n] < y[n]) return -1;
+        else {
+            if(x > y) return 1;
+            else if(x < y) return -1;
+        }
+    })
+}
