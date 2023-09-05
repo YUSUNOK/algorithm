349d58c37848ed72852846605a5819776e41b32c
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-09-05 21:46:10 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
+++ 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
@@ -1,3 +1,10 @@
 const solution = (n) => {
-    return Number([...String(n)].sort((x, y) => y - x).join(''));
+    return Number([...String(n)].sort((x, y) => {
+        if (Number(x) < Number(y)) { 
+            return 1;
+        }else if(Number(x) > Number(y)) {
+            return -1;
+        }
+        return 0;
+    }).join(''));
 }
