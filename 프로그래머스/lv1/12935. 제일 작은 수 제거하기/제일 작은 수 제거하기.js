012aafae10312891f53eb76fc26a602c9abc8493
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-09-05 23:03:49 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.04 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,11 +1,5 @@
 const solution = (arr) => {
-    if(arr.length === 1) return [-1];
-    let smallValueAndIndex = [arr[0], 0];
-    for(let i = 1; i < arr.length; i++) {
-        if(smallValueAndIndex[0] > arr[i]) {
-            smallValueAndIndex = [arr[i], i];
-        }
-    }
-    arr.splice(smallValueAndIndex[1], 1);
-    return arr;
+    let minValue = Math.min(...arr);
+    arr.splice(arr.indexOf(minValue), 1);
+    return arr.length === 0 ? [-1] : arr;
 }
