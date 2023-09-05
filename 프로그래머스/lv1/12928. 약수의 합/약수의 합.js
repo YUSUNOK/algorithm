067b90c262a958b9f112bb563cd0b54b4850bd02
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-09-05 21:03:17 +0900

    [level 1] Title: 약수의 합, Time: 0.13 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
@@ -1,9 +1,12 @@
 const solution = (n) => {
-    let answer = 0;
-    for (let i = 1; i <= Math.sqrt(n); i++) {
-        if (n % i === 0) {
-            i === Math.sqrt(n) ? answer += i : answer += i + (n / i);
+    let result = 0;
+    for (let i = 1; i < Math.sqrt(n); i++) {
+        if(n % i === 0) {
+            result += i + n/i;
         }
     }
-    return answer;
+    if (Number.isInteger(Math.sqrt(n))) {
+        result += Math.sqrt(n);
+    }
+    return result;
 }
