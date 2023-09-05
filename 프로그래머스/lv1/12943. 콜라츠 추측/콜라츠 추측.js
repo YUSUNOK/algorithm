b4c3d83125c17284106404ce906cf841935fbe06
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-09-05 22:47:32 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,9 +1,8 @@
 const solution = (n) => {
     if(n === 1) return 0;
-    for (let i = 1; i <= 500; i++) {
-        if(n % 2 === 0) n /= 2;
-        else n = n*3 + 1;
-        
+    for (let i = 1; i<=500; i++) {
+        const isEven = n % 2 === 0;
+        isEven ? n /= 2 : n = n*3+1;
         if(n === 1) return i;
     }
     return -1;
