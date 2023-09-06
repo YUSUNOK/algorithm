f56bac2f91453c3138fb14994362434f9b63c105
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-09-06 13:20:29 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,7 +1,13 @@
 const solution = (s) => {
-    const filterArr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
     const sLength = s.length;
-    if(!(sLength === 4 || sLength === 6)) return false;
-    const fiterSLength = [...s].filter((x) => filterArr.includes(x)).length;
-    return sLength === fiterSLength ? true: false;
+    if (sLength !== 4 || sLength !== 6) return false;
+    
+    const sFilterLength = [...s].filter(x => x !== 'e' || Number.isInteger(Number(x)));
+    
+    console.log(sFilterLength);
+    
+    return sLength === sFilterLength ? true : false;
+    
+    
+    
 }
