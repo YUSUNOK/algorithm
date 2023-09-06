83edf2bfc01624e9f4dd1b5f6f343bff099a72e6
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2023-09-06 09:26:42 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,11 +1,8 @@
-const numberOfDivisors = (x) => {
-   return Array(x).fill(1).map((x, i) => x+i).filter((number) => x % number === 0).length;
-}
-
 const solution = (left, right) => {
     let answer = 0;
-    for(let i =left; i<= right; i++) {
-        numberOfDivisors(i) % 2 === 0 ? answer += i : answer -= i;
+    for(let i = left; i <= right; i++) {
+        const isOdd = Number.isInteger(Math.sqrt(i));
+        isOdd ? answer -= i : answer += i;
     }
     return answer;
 }
